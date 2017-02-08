21e982b0c9acc4c0506eb4e2be31b055d5329ff6
Andrea Bizzotto <bizz84@gmail.com>
2017-02-08 21:00:14 +0000

    Initial Commit

---
+++ PopSpringViews/PopSpringViews.h
@@ -0,0 +1,19 @@
+//
+//  PopSpringViews.h
+//  PopSpringViews
+//
+//  Created by Andrea Bizzotto on 08/02/2017.
+//  Copyright © 2017 musevisions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopSpringViews.
+FOUNDATION_EXPORT double PopSpringViewsVersionNumber;
+
+//! Project version string for PopSpringViews.
+FOUNDATION_EXPORT const unsigned char PopSpringViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopSpringViews/PublicHeader.h>
+
+
